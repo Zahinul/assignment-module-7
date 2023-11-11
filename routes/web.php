9538ce766733ffdef09a42d820f45d318d9d0c05
Md. Zahinul Karim <zk.ajmain@gmail.com>
2023-11-11 19:49:38 +0600

    Updated web.php

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,5 @@ use App\Http\Controllers\UserController;
 Route::get('/', function () {
     return view('welcome');
 });
-//The route with a get request that handles the message from a function sayHello in UserController that displays the message on a web browser on localhost/greet
+//The route with a get request that handles the message from a function sayHello in UserController that displays the message on a web browser on localhost:8000/greet
 Route::get('/greet',[UserController::class,'sayHello']);
